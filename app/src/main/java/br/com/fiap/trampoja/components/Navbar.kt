ba3101aad048197728d6810ccc032e3476b228e7
matheusadriel2 <matheusadrieldsc@gmail.com>
2025-09-06 22:44:02 -0300

    feat: create navbar

---
+++ app/src/main/java/br/com/fiap/trampoja/components/Navbar.kt
@@ -0,0 +1,16 @@
+package br.com.fiap.trampoja.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.trampoja.ui.theme.TrampojaTheme
+
+@Composable
+fun Navbar() {
+
+}
+
+@Preview
+@Composable
+private fun NavbarPreview() {
+    TrampojaTheme { Navbar() }
+}
