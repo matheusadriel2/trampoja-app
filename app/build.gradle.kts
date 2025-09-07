b6657c5bb3c8b3d665f458a357f58cca531ca3af
matheusadriel2 <matheusadrieldsc@gmail.com>
2025-09-07 15:23:43 -0300

    chore: update Gradle dependencies and configuration
    - Added Material3 theme support
    - Updated Compose and lifecycle libraries
    - Synced gradle.properties with project setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.compose.material3:material3:1.3.2")
     implementation("androidx.compose.material:material-icons-extended:1.6.8")
     implementation("androidx.navigation:navigation-compose:2.9.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.3")
